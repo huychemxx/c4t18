6f269c88baa4120e46ba98e4500c5f6048adcdd7
huychemxx <lgcoten123@gmail.com>
2019-05-26 17:10:24 +0700

    new file

---
+++ session2/in_intro.py
@@ -0,0 +1,12 @@
+from turtle import *
+shape("turtle")
+for x in range(6):
+    for i in range(360):
+        forward(2)
+        left(1)
+    left(45)
+speed(0)
+        
+
+    
+mainloop()
---
+++ session5/oto.py
@@ -0,0 +1,12 @@
+x = int(input("mot thang trong nam"))
+
+
+if x < 4:
+    print("spring")
+elif x < 7:
+    print("summer")
+elif x < 10:
+    print("autumn")
+else:
+    print("winter")
+    
